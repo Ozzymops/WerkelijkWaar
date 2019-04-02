0e44851f035dad36e8f9ca3e53f278119668ba46
Justin Muris <1736302muris@zuyd.nl>
2019-04-02 15:50:44 +0200

    Check login

---
+++ WerkelijkWaar/Classes/DatabaseQueries.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WerkelijkWaar.Classes
+{
+    public class DatabaseQueries
+    {
+        private string connectionString = ConfigurationManager.AppSettings["connectionString"];
+
+        // CRUD
+        #region CREATE
+        public bool CheckLogin(string username, string password)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("LogIn", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.Add(new SqlParameter("@pUsername", username));
+                command.Parameters.Add(new SqlParameter("@pPassword", password));
+
+                var output = new SqlParameter("@responseMessage", System.Data.SqlDbType.NVarChar);
+                output.Direction = System.Data.ParameterDirection.Output;
+                output.Size = 255;
+                command.Parameters.Add(output);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    if (command.Parameters["@responseMessage"].Value.ToString().Contains("Success"))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+        #endregion
+
+        #region READ
+        #endregion
+
+        #region UPDATE
+        #endregion
+
+        #region DELETE
+        #endregion
+    }
+}
