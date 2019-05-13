30c52f373161ef80789665e50fdbfc156d504997
justinmuris <justinmuris@gmail.com>
2019-05-13 11:07:14 +0200

    Info screen finished

---
+++ WerkelijkWaar/wwwroot/js/gameInfoScreen.js
@@ -0,0 +1,26 @@
+﻿// Swaps GameInfo screen divs on button press.
+var currentScreen = 0;
+
+$('#swapButton').click(function () {
+    // Swap variable
+    if (currentScreen == 0) {
+        currentScreen = 1;
+    }
+    else {
+        currentScreen = 0;
+    }
+
+    // Swap elements
+    if (currentScreen == 0) {
+        // info-1
+        document.getElementById("info-1").style.display = "block";
+        document.getElementById("info-2").style.display = "none";
+        $("#swapButton").prop('value', 'Meer over het spel');
+    }
+    else {
+        // info-2
+        document.getElementById("info-1").style.display = "none";
+        document.getElementById("info-2").style.display = "block";
+        $("#swapButton").prop('value', 'Hoe verloopt het spel?');
+    }
+});
